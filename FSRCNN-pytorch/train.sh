f85dabc42aa8694df71467cb93347bbe5228a83d
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-07-29 11:39:38 +0200

    Update train.sh

--- FSRCNN-pytorch/train.sh
+++ FSRCNN-pytorch/train.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-#SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -c 1
-#SBATCH --mem=30G
-#SBATCH -p GPU
-#SBATCH --gres=gpu:gtxp:1
-#SBATCH -t 3-0   # 3-0 (3 jours)
-#SBATCH -o FSRCNN_train_mouse.out
-#SBATCH -J FSR_train
-
-source /home_expes/tools/python/Python-3.7.1-ubuntu_gpu/bin/activate
-
-srun python /home/jhr11385/FSRCNN-pytorch/train.py
+python ./train.py
