25679932f5a566e5910118bf222826fe5d05ad35
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-07-29 12:40:06 +0200

    Update Evaluate_PSNR_SSIM.m

--- Evaluate_PSNR_SSIM.m
+++ Evaluate_PSNR_SSIM.m
@@ -3,66 +3,53 @@ function Evaluate_PSNR_SSIM()
 clear all; close all; clc
 
 %% set path
-degradation = 'BI'; % BI, BD
-methods = {'RCAN', 'RCANplus'};
-dataset = {'p2p_denoise'};
+path_to_dataset = "./"
+dataset = {'pix2pix_result'};
 ext = {'*.jpg', '*.png', '*.bmp'};
-num_method = length(methods);
 num_set = length(dataset);
-record_results_txt = ['PSNR_SSIM_Results_', degradation,'_model.txt'];
+record_results_txt = ['PSNR_SSIM_Results_model.txt'];
 results = fopen(fullfile(record_results_txt), 'wt');
-filename_mat = "./p2p_denoise.mat"
+filename_mat = "./p2p_result.mat"
 
-if strcmp(degradation, 'BI') 
-    scale_all = 2
-else
-    scale_all = 3;
-end
+scale_all = 2
 
-for idx_method = 1:num_method
-    
-    for idx_set = 1:num_set
-        fprintf(results, '**********************\n');
-        fprintf(results, 'Method_%d: %s; Set: %s\n', idx_method, methods{idx_method}, dataset{idx_set});
-        fprintf('**********************\n');
-        fprintf('Method_%d: %s; Set: %s\n', idx_method, methods{idx_method}, dataset{idx_set});
-        for scale = scale_all
-            filepaths = [];
-            for idx_ext = 1:length(ext)
-                filepaths = cat(1, filepaths, dir(fullfile('./SR', 'BI' , 'RCAN' , 'p2p_denoise', ext{idx_ext})));
-            end
-            PSNR_all = zeros(1, length(filepaths));
-            SSIM_all = zeros(1, length(filepaths));
-            for idx_im = 1:length(filepaths)
-                %name_HR = filepaths(idx_im).name;
-                name_SR = filepaths(idx_im).name;
-                %name_SR = strrep(name_HR, '.png', '_fake_B.png');
-                name_HR = strrep(name_SR,'_fake_B.png','.png');
-                im_HR = imread(fullfile('./HR', 'RCAN_MOUSE', name_HR));
-                im_SR = imread(fullfile('./SR', degradation, [methods{idx_method}], dataset{idx_set}, name_SR));
-                % change channel for evaluation
-                if 3 == size(im_HR, 3)
-                    im_HR_YCbCr = single(rgb2ycbcr(im2double(im_HR)));
-                    im_HR_Y = im_HR_YCbCr(:,:,1);
-                    im_SR_YCbCr = single(rgb2ycbcr(im2double(im_SR)));
-                    im_SR_Y = im_SR_YCbCr(:,:,1);
-                else
-                    im_HR_Y = single(im2double(im_HR));
-                    im_SR_Y = single(im2double(im_SR));
-                end
-                % calculate PSNR, SSIM
-                [PSNR_all(idx_im), SSIM_all(idx_im)] = Cal_Y_PSNRSSIM(im_HR_Y*255, im_SR_Y*255, scale, scale);
-                fprintf(results, 'x%d %d %s: PSNR= %f SSIM= %f\n', scale, idx_im, name_SR, PSNR_all(idx_im), SSIM_all(idx_im));
-                fprintf('x%d %d %s: PSNR= %f SSIM= %f\n', scale, idx_im, name_SR, PSNR_all(idx_im), SSIM_all(idx_im));
-            end
-            fprintf(results, '--------Mean--------\n');
-            fprintf('--------Mean--------\n');
-            fprintf(results, 'x%d: PSNR= %f SSIM= %f\n', scale, mean(PSNR_all), mean(SSIM_all));
-            fprintf('x%d: PSNR= %f SSIM= %f\n', scale, mean(PSNR_all), mean(SSIM_all));
-            save(filename_mat,'PSNR_all','SSIM_all')
+for idx_set = 1:num_set
+
+    for scale = scale_all
+        filepaths = [];
+        for idx_ext = 1:length(ext)
+            filepaths = cat(1, filepaths, dir(fullfile(path_to_dataset, dataset{idx_set}, ext{idx_ext})));
         end
+        PSNR_all = zeros(1, length(filepaths));
+        SSIM_all = zeros(1, length(filepaths));
+        for idx_im = 1:length(filepaths)
+            name_SR = filepaths(idx_im).name;
+            name_HR = strrep(name_SR,'_fake_B.png','.png'); % Important to change depending on image name of SR
+            im_HR = imread(fullfile('./datasets/MOUSE/HR/Test', name_HR));
+            im_SR = imread(fullfile(path_to_dataset, dataset{idx_set}, name_SR));
+            % change channel for evaluation
+            if 3 == size(im_HR, 3)
+                im_HR_YCbCr = single(rgb2ycbcr(im2double(im_HR)));
+                im_HR_Y = im_HR_YCbCr(:,:,1);
+                im_SR_YCbCr = single(rgb2ycbcr(im2double(im_SR)));
+                im_SR_Y = im_SR_YCbCr(:,:,1);
+            else
+                im_HR_Y = single(im2double(im_HR));
+                im_SR_Y = single(im2double(im_SR));
+            end
+            % calculate PSNR, SSIM
+            [PSNR_all(idx_im), SSIM_all(idx_im)] = Cal_Y_PSNRSSIM(im_HR_Y*255, im_SR_Y*255, scale, scale);
+            fprintf(results, 'x%d %d %s: PSNR= %f SSIM= %f\n', scale, idx_im, name_SR, PSNR_all(idx_im), SSIM_all(idx_im));
+            fprintf('x%d %d %s: PSNR= %f SSIM= %f\n', scale, idx_im, name_SR, PSNR_all(idx_im), SSIM_all(idx_im));
+        end
+        fprintf(results, '--------Mean--------\n');
+        fprintf('--------Mean--------\n');
+        fprintf(results, 'x%d: PSNR= %f SSIM= %f\n', scale, mean(PSNR_all), mean(SSIM_all));
+        fprintf('x%d: PSNR= %f SSIM= %f\n', scale, mean(PSNR_all), mean(SSIM_all));
+        save(filename_mat,'PSNR_all','SSIM_all')
     end
 end
+
 fclose(results);
 
 end
