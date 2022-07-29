9816ee2be892492e4bd228b03ee7159c44656825
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-07-29 11:15:51 +0200

    Create test.sh

---
+++ pix2pix/test.sh
@@ -0,0 +1,1 @@
+python ./test.py --dataroot ../datasets/MOUSE/Train --BPNN_mode False --batch_size 1 --input_nc 1 --output_nc 1 --netG unet_256 --name pix2pix_testing --name_checkpoint MOUSE_checjpoints --model pix2pix --direction AtoB --proprocess None
