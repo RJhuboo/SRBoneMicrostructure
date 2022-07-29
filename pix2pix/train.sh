e2c3bfc97b6e6c3857d4d2d9ed0096f55021b3e4
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-07-29 11:11:15 +0200

    Update train.sh

--- pix2pix/train.sh
+++ pix2pix/train.sh
@@ -1,1 +1,1 @@
-python ?/processing.py --dataroot ./datasets/MOUSE/Train --noise_sigma 0.1 --BPNN_mode False --batch_size 8 --input_nc 1 --output_nc 1 --netG unet_256 --name MOUSE_big_data --n_epochs 20 --n_epochs_decay 30 --model pix2pix --direction AtoB --preprocess None
+python ?/processing.py --dataroot ./datasets/MOUSE/Train --noise_sigma 0.1 --BPNN_mode False --batch_size 8 --input_nc 1 --output_nc 1 --netG unet_256 --name pix2pix_training --n_epochs 20 --n_epochs_decay 30 --model pix2pix --direction AtoB --preprocess None
