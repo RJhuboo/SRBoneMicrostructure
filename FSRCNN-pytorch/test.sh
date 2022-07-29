cde32121f4845b4e2257b7b65f25ce82421c31bc
RJhuboo <88443856+RJhuboo@users.noreply.github.com>
2022-07-29 11:44:31 +0200

    Update test.sh

--- FSRCNN-pytorch/test.sh
+++ FSRCNN-pytorch/test.sh
@@ -1,1 +1,1 @@
-python ./test.py --weights-file ./FSRCNN-output/MOUSE_big_data_x2/best.pth --image-dir ../datasets/MOUSE/LR/Test --label-dir ../datasets/MOUSE/HR/Test --output-dir ./FSRCNN-output/FSRCNN_testing
+python ./test.py --weights-file ./FSRCNN-output/MOUSE_FSRCNN_x2/best.pth --image-dir ../datasets/MOUSE/LR/Test --label-dir ../datasets/MOUSE/HR/Test --output-dir ./FSRCNN-output/FSRCNN_testing
